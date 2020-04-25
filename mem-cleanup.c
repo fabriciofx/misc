ca1d040f082c4dad825ca9b20d0c0eba56f4f602
Fabrício Cabral <fabriciofx@gmail.com>
2020-04-25 18:21:13 -0300

    more tests with cleanup function

--- mem-cleanup.c
+++ mem-cleanup.c
@@ -1,21 +1,13 @@
 #include <stdlib.h>  
 #include <stdio.h>  
   
-#define autofree __attribute((cleanup(__autofree)))  
+#define smart __attribute((cleanup(__smart)))  
 
-typedef struct _Person Person;
-
-struct _Person
-{
-    int age;
-    char *name;
-};
-
-void __autofree(void *p)  
+void __smart(void *p)  
 {  
     void **_p = (void**)p;  
   
-    printf("free -> %p\n", *_p);  
+    printf("free: %p\n", *_p);  
     free(*_p);  
 }  
   
@@ -27,20 +19,46 @@ void *myalloc(size_t sz)
         fprintf(stderr, "malloc failed.\n");  
         exit(1);  
     }  
-    printf("malloc -> %p\n", ptr);  
+    printf("malloc: %p\n", ptr);  
   
     return ptr;  
 }  
-  
+ 
+typedef struct _Person Person;
+
+struct _Person
+{
+    int age;
+    char *name;
+};
+
+Person* person_new(char *name, int age)
+{
+    Person* p = myalloc(sizeof(Person));
+    p->name = name;
+    p->age = age;
+    return p;
+}
+
+void person_print(Person *person)
+{
+    printf("Name: %s, Age: %d\n", person->name, person->age);
+}
+
+ 
 int main(int argc, char **argv)  
 {  
-    autofree char *x = myalloc(32);
+    smart char *x = myalloc(32);
     {  
-        autofree int *y = myalloc(64);  
+        smart int *y = myalloc(64);  
   
         printf("y = %p\n", y);  
     }  
     printf("x = %p\n", x);  
-  
+    
+    smart Person* p = person_new("fabricio", 40);
+    person_print(p);
+    printf("Name2: %s, Age2: %d\n", p->name, p->age);
+
     return 0;
 }  
