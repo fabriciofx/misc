180cb305c64763a34b3eb48d2c53d260c25bfb94
Fabrício Cabral <fabriciofx@gmail.com>
2020-10-07 22:33:31 -0300

    Calculates the complexity and plot graph

--- python/complexity.py
+++ python/complexity.py
@@ -10,6 +10,7 @@ input = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160,
 output = []
 
 # Perform the algorithm
+print("n;time")
 for n in input:
     x = []
 
@@ -19,7 +20,7 @@ for n in input:
     start_time = time.time()
     for i in range(1, n - 2):
         for j in range(1, 2 * n):
-            if (i  % 2) == 0:
+            if (i  % 2) != 0:
                 for k in range(1, n ** 2):
                     x[k - 1] = x[i - 1] * x[i - 1]
             else:
@@ -31,11 +32,10 @@ for n in input:
     end_time = time.time()
     elapsed = end_time - start_time
     output.append(elapsed)
-    print("n = ", n, " time: ", elapsed)
+    print(n, ";", elapsed)
 
 # Plot the graph
 plt.plot(input, output)
-#plt.plot(input, label="n^3")
 plt.xlabel('n')
 plt.ylabel('time')
 plt.title('Complexity')
